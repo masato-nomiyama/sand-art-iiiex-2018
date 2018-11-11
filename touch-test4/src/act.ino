cacc6db9926ec07ca112e864c59d3b2ad12baa72
Masato Nomiyama <nomiyama@cyber.t.u-tokyo.ac.jp>
2018-11-12 01:33:42 +0900

    make it easier to consider actuation

---
+++ touch-test4/src/act.ino
@@ -0,0 +1,61 @@
+// Copyright (C) 2018-Present Masato Nomiyama
+
+float actWhenClose(int place) {
+  float lastPhase = 1.0;
+  switch(place) {
+    case 0:
+    case 1:
+    case 2:
+    case 3: {
+      // Set phases
+      float waveLengthRates[4] = {
+        1.0 - propagateRate * 0.5,
+        1.0,
+        1.0,
+        1.0 - propagateRate * 0.5
+      };
+      float phases[4] = {};
+      lastPhase = setPhases(
+        phases,
+        waveLengthRates,
+        sizeof(phases) / sizeof(float)
+      );
+
+      // Drive magnets
+      int pinIndex1[] = { 0 };
+      drive(triangleSpike, phases[0], 0.05,
+        pinIndex1, sizeof(pinIndex1) / sizeof(int));
+      int pinIndex2[] = { 1 };
+      drive(triangleSpike, phases[1], 1.0,
+        pinIndex2, sizeof(pinIndex2) / sizeof(int));
+      int pinIndex3[] = { 3 };
+      drive(triangleSpike, phases[2], 1.0,
+        pinIndex3, sizeof(pinIndex3) / sizeof(int));
+      int pinIndex4[] = { 2 };
+      drive(triangleSpike, phases[3], 0.05,
+        pinIndex4, sizeof(pinIndex4) / sizeof(int));
+
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return lastPhase;
+}
+
+float actWhenTouched(int place) {
+  float lastPhase = 1.0;
+  switch(place) {
+    case 0:
+    case 1:
+    case 2:
+    case 3: {
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return lastPhase;
+}
--- touch-test4/src/touch-test4.ino
+++ touch-test4/src/touch-test4.ino
@@ -33,6 +33,7 @@ void drive(
   int *drivePins,
   unsigned int phaseNum
 );
+void execute(float (*act)(int));
 
 void setup() {
   Serial3.begin(9600);
@@ -86,59 +87,11 @@ void loop() {
       break;
     }
     case ACT_WHEN_CLOSE: {
-      float lastPhase = 1.0;
-      switch(place) {
-        case 0:
-        case 1:
-        case 2:
-        case 3: {
-          // Set phases
-          float waveLengthRates[4] = {
-            1.0 - propagateRate * 0.5,
-            1.0,
-            1.0,
-            1.0 - propagateRate * 0.5
-          };
-          float phases[4] = {};
-          lastPhase = setPhases(
-            phases,
-            waveLengthRates,
-            sizeof(phases) / sizeof(float)
-          );
-
-          // Drive magnets
-          int pinIndex1[] = { 0 };
-          drive(triangleSpike, phases[0], 0.05,
-            pinIndex1, sizeof(pinIndex1) / sizeof(int));
-          int pinIndex2[] = { 1 };
-          drive(triangleSpike, phases[1], 1.0,
-            pinIndex2, sizeof(pinIndex2) / sizeof(int));
-          int pinIndex3[] = { 3 };
-          drive(triangleSpike, phases[2], 1.0,
-            pinIndex3, sizeof(pinIndex3) / sizeof(int));
-          int pinIndex4[] = { 2 };
-          drive(triangleSpike, phases[3], 0.05,
-            pinIndex4, sizeof(pinIndex4) / sizeof(int));
-
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-      update(lastPhase);
+      execute(actWhenClose);
       break;
     }
     case ACT_WHEN_TOUCHED: {
-      float lastPhase = 1.0;
-      switch(place) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-          break;
-      }
-      update(lastPhase);
+      execute(actWhenTouched);
       break;
     }
     case REST: {
@@ -163,6 +116,12 @@ float setPhases(float *phases, float *waveLengthRates, unsigned int phaseNum) {
   return phases[phaseNum - 1];
 }
 
+void start() {
+  pwmPeriod = t;
+  wavePeriod = t;
+  propagateOffset = 0;
+}
+
 void drive(
   float (*wave)(float),
   float phase,
@@ -181,13 +140,8 @@ void drive(
   }
 }
 
-void start() {
-  pwmPeriod = t;
-  wavePeriod = t;
-  propagateOffset = 0;
-}
-
-void update(float lastPhase) {
+void execute(float (*act)(int)) {
+  float lastPhase = act(place);
   if (t - pwmPeriod >= pwmLength) {
     pwmPeriod = t;
   }
